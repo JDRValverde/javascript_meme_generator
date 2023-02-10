263517a647998022a7fbfd592c8352426931245a
JDRV <jodaniel09@gmail.com>
2023-02-10 00:02:15 -0600

    Add source code

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="/logo192.png" className="header--image" alt="logo" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">JDRV - React</h4>
+        </header>
+    )
+}
