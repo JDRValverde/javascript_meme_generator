5c2e65b210a142963dc3ea90491a251a0943f656
JDRV <jodaniel09@gmail.com>
2023-02-16 23:47:11 -0600

    Add props to components

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,18 +1,21 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
 
-    function handleClick() {
-        console.log("I was clicked!")
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
     }
-
     return (
-        <main className="form">
-            <form className="form">
+        <main>
+            <div className="form">
+                <p>{url}</p>
                 <input type="text" className="form--input" placeholder="Top Text"></input>
                 <input type="text" className="form--input" placeholder="Bottom Text"></input>
-                <input type="submit" className="form--button" value="New image"></input>
-            </form>
+                <button className="form--button" onClick={getMemeImage}></button>
+            </div>
         </main>
     )
 }
