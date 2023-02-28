6277c422ed9b8528b3d1165936ea83857c20e3c1
JDRV <jodaniel09@gmail.com>
2023-02-27 23:57:23 -0600

    Fix data retrival

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,20 +2,20 @@ import React from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
-
+    const [memeImage, setMemeImage] = React.useState("")
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
     }
     return (
         <main>
             <div className="form">
-                <p>{url}</p>
                 <input type="text" className="form--input" placeholder="Top Text"></input>
                 <input type="text" className="form--input" placeholder="Bottom Text"></input>
-                <button className="form--button" onClick={getMemeImage}></button>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
+            <img src={memeImage} alt="Memes" className="meme--image" />
         </main>
     )
 }
