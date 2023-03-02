d40b7df99ec5b187af8580d5f042523dc2a1f54b
JDRV <jodaniel09@gmail.com>
2023-03-01 23:22:50 -0600

    Add meme state and props

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,11 +2,26 @@ import React from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("")
+
+
+    //const [memeImage, setMemeImage] = React.useState("https://i.imgflip.com/30b1gx.jpg")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/30b1gx.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
     return (
         <main>
@@ -15,7 +30,7 @@ export default function Meme() {
                 <input type="text" className="form--input" placeholder="Bottom Text"></input>
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
-            <img src={memeImage} alt="Memes" className="meme--image" />
+            <img src={meme.randomImage} alt="Memes" className="meme--image" />
         </main>
     )
 }
